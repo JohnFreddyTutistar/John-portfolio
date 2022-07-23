1f70d031d8e14552b7873c7922f7c8d059f4ec5f
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-07-23 01:45:26 -0500

    ajustando tamaño del logo t

--- js/appform.js
+++ js/appform.js
@@ -18,7 +18,7 @@ const pintarMensajeError = (errores) => {
     errores.forEach(item => {
         item.tipo.classList.remove("d-none")
         item.tipo.textContent = item.msg;
-        
+
     })
 
 }
@@ -40,44 +40,45 @@ btnSubmit.addEventListener("click", e => {
 
     //validacion nombre de usuario
 
-    if(!regUserName.test(userName.value) || !userName.value.trim()){
-        userName.classList.add("is-invalid") 
+    if (!regUserName.test(userName.value) || !userName.value.trim()) {
+        userName.classList.add("is-invalid")
         errores.push({
             tipo: alertName,
             msg: "Invalid format in the name field"
         })
-    } else{
+    } else {
         userName.classList.remove("is-invalid")
         userName.classList.add("is-valid")
-        alertName.classList.add("d-none")
+        alertName.classList.add("d-none")     
     }
 
     //validacion email de usuario
-    if(!regUserEmail.test(userEmail.value) || !userEmail.value.trim()){
+    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
         userEmail.classList.add("is-invalid")
         errores.push({
             tipo: alertEmail,
             msg: "write a valid email"
         })
-    } else{
+    } else {
         userEmail.classList.remove("is-invalid")
         userEmail.classList.add("is-valid")
         alertEmail.classList.add("d-none")
     }
 
     //validacion mensaje
-    if(!regMessage.test(message.value) || !message.value.trim()){
+    if (!regMessage.test(message.value) || !message.value.trim()) {
         message.classList.add("is-invalid")
         errores.push({
             tipo: alerMessage,
             msg: "write a message"
         })
-    } else{
+    } else {
         message.classList.remove("is-invalid")
         message.classList.add("is-valid")
         alerMessage.classList.add("d-none")
+
     }
-    if(errores.length != 0){
+    if (errores.length != 0) {
         pintarMensajeError(errores)
         return
     }
